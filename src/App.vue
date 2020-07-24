d805fd565ede61dfc1f6e07c9df0ba4678761c47
Tereshka <tereshka07@gmail.com>
2020-07-25 03:18:40 +0900

    remove unused files

--- src/App.vue
+++ src/App.vue
@@ -3,14 +3,9 @@
     <el-container>
       <el-header height="100">
         <header-menu />
-        <!-- <div id="nav">
-          <router-link to="/">Home</router-link> |
-          <router-link to="/about">About</router-link>
-        </div> -->
       </el-header>
       <el-main>
         <tabs-container />
-        <!-- <router-view/> -->
       </el-main>
     </el-container>
   </div>
